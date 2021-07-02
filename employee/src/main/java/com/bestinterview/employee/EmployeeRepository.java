435234e58e3f2658b21bc8ce91bd701406594ef3
Prashant Srivastava <prashant.srivastava@MACPRO251.local>
2021-07-02 16:32:04 +0530

    sas

--- employee/src/main/java/com/bestinterview/employee/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bestinterview.employee;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<EmployeeDTO, Integer>{
-
-}
